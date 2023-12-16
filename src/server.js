ccb3406e16eb1e3cfb43d33d1b211e70e365c558
Ethan-Marchionini <98862161+Ethan-Marchionini@users.noreply.github.com>
2023-12-16 13:33:06 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(port, hostname: () => {
+  console.log('Started on port ${port} ');
+});
+
+
 
